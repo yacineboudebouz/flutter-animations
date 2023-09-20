9fc61664c08352ac3d6eb4500a3d3537668d05eb
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-09-20 15:49:56 +0200

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
-import 'package:animation_starter_code/implicit_examples/animated_shopping_cart_button.dart';
 import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
 import 'package:flutter/material.dart';
 
