52b6cf2d646abf0bcc4f55aea4328ebd13f868c3
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-09-21 16:13:01 +0200

    controlled animation

--- lib/explicit_examples/login_animation.dart
+++ lib/explicit_examples/login_animation.dart
@@ -1,40 +1,78 @@
 import 'package:flutter/material.dart';
 
-class LoginScreenAnimation extends StatelessWidget {
+class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
 
   @override
+  State<LoginScreenAnimation> createState() => _LoginScreenAnimationState();
+}
+
+class _LoginScreenAnimationState extends State<LoginScreenAnimation>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> logoFadeAnimation;
+  late AnimationController controllerIn;
+  late Animation<Offset> slideAnimation;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controllerIn =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slideAnimation = Tween(begin: const Offset(-1, 0), end: const Offset(0, 0))
+        .animate(CurvedAnimation(parent: controllerIn, curve: Curves.bounceIn));
+
+    logoFadeAnimation = Tween(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: controllerIn, curve: Curves.bounceIn));
+    scaleAnimation = Tween(begin: 0.0, end: 1.0).animate(controllerIn);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    controllerIn.forward();
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoFadeAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                    ),
+            SlideTransition(
+              position: slideAnimation,
+              child: ScaleTransition(
+                scale: scaleAnimation,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            controllerIn.repeat();
+                          });
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PulsatingCircleAnimation(),
+      home: const LoginScreenAnimation(),
     );
   }
 }
